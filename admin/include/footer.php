1f8266cb5ac3bf6594b4e62e3d07abfebad14c4f
Utkarsh Brajnil <brajnilutkarsh@gmail.com>
2020-06-08 21:28:43 +0530

    :pencil: final commit

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2019 VITCC  CMS </b> All rights reserved.
+		</div>
+	</div>
---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              2019 - any queries contact support@vit.ac.in
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
